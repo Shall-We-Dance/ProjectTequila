a070d063e6a83fbe5a49e02494526b24599032b3
ShallWeTalk <91458979+Shall-We-Dance@users.noreply.github.com>
2021-12-16 21:25:09 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Project Tequila</title>
+  <meta name="description" content="这里是PT服务器，Welcome to Project Tequila!">
+  <link rel="shortcut icon" href="src/PT-logo.png" />
+  <link rel="bookmark" href="src/PT-logo.png" />
+  <link href="https://fonts.googleapis.com/css?family=Quicksand:400,700" rel="stylesheet">
+  <link rel="stylesheet" href="css/normalize.min.css">
+  <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <!--<link rel="prefetch" href="src/map.png" />-->
+  <link rel="prefetch" href="picture" />
+  <link rel="prefetch" href="donate" />
+
+<?php
+    $counter=file_get_contents('./src/prefetch_images.txt', $counter);
+    echo $counter;
+
+
+?>
+
+<!--这是用于流量统计的js -->
+  <script>
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?20a5c847be75cb0cfbb31476570fe424";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
+  </script>
+</head>
+
+<body>
+  <div class="logo" id="logo">
+    <img src="src/pic.png" align="center" width="380px" >
+  </div>
+  <h1>Welcome to Project Tequila</h1>
+  <p>Project Tequila 是一个 <code>MineCraft服务器</code></p>
+
+
+
+<div class="container collapse">
+  <details>
+    <summary><i class="fas fa-info-circle"></i> 关于我们</summary>
+    <div class="details-wrapper">
+      <div class="details-styling">        
+        <p>Project Tequila 始于2019年9月，初为 <code>南开大学<sup>sky</sup></code> 几位同学的内部服务器，经不断升级完善，目前已对外免费开放。</p>
+        <p><h3>价值观</h3><p>
+        <p><h4><b><code>多元</code> <code>包容</code> <code>自由</code> <code>友善</code> <code>合作</code></b></h4><p>
+        <p><h3>目标</h3></p>
+        <p>一个轻松舒适的地方，不只是游戏。</p>
+        <p>任何被邀请者或通过简单测试的朋友都可以加入，无论性别、婚姻、生理、种族、地域或宗教信仰。</p>
+        <p>仅有的要求是待人友善，不攻击它人，并遵守<code>服规</code>。</p>
+        <p><h3>属性</h3></p>
+        <p>
+        <table border="0">
+            <tr>
+                <th align="center">&nbsp;&nbsp;&nbsp;&nbsp;属性&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                <th align="center">&nbsp;&nbsp;&nbsp;&nbsp;生存服&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                <th align="center">&nbsp;&nbsp;&nbsp;&nbsp;困难服&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                <th align="center">&nbsp;&nbsp;&nbsp;&nbsp;创造服&nbsp;&nbsp;&nbsp;&nbsp;</th>
+            </tr>
+            <tr>
+                <td align="center">版本</td>
+                <td align="center">1.16.1</td>
+                <td align="center">1.16.5(兼容)</td>
+                <td align="center">1.16.1</td>
+            </tr>
+            <tr>
+                <td align="center">死亡掉落</td>
+                <td align="center">否</td>
+                <td align="center">是</td>
+                <td align="center">否</td>
+            </tr>
+            <tr>
+                <td align="center">OP权限</td>
+                <td align="center">有</td>
+                <td align="center">无</td>
+                <td align="center">有</td>
+            </tr>
+            <tr>
+                <td align="center">难度</td>
+                <td align="center">困难</td>
+                <td align="center">困难</td>
+                <td align="center">和平</td>
+            </tr>
+            <tr>
+                <td align="center">幻翼</td>
+                <td align="center">无</td>
+                <td align="center">有</td>
+                <td align="center">无</td>
+            </tr>
+            <tr>
+                <td align="center">生物破坏</td>
+                <td align="center">有</td>
+                <td align="center">有</td>
+                <td align="center">有</td>
+            </tr>
+            <tr>
+                <td align="center">苦力怕爆炸</td>
+                <td align="center">无</td>
+                <td align="center">有</td>
+                <td align="center">有</td>
+            </tr>
+            <tr>
+                <td align="center">火焰蔓延</td>
+                <td align="center">无</td>
+                <td align="center">有</td>
+                <td align="center">无</td>
+            </tr>
+        </table>
+        </p>
+      </div>
+    </div>
+  </details>
+  <details>
+    <summary><i class="fas fa-book"></i> 服规</summary>
+    <div class="details-wrapper">
+      <div class="details-styling">
+         <p>
+        1、<code>反对任何形式的歧视。</code>我们禁止任何带有歧视或攻击性的行为，包括但不限于性别、婚姻、生理、种族、地域或宗教信仰；
+        <p>2、<code>私人财产神圣不可侵犯。</code>未取得他人同意的前提下，不可更改其的所属物，包括但不限于方块与物品；<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;此条例保护以下情形：<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;（1）在私人建筑区域内的；<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;（2）在公共区域但做出声明的；
+        <p>3、<code>禁止危害公共安全。</code>禁止一切卡服、炸服行为；
+        <p>4、<code>保护机器，安全生产。</code>认真阅读机械说明并且谨慎操作，若出现问题请在群内通知；
+        <p>5、<code>友好互助。</code>我们建议共享无用材料。取用不属于自己的箱子中的物品时，建议放置木牌说明取用者id与取用内容，减少误会。
+        <p>2021.02.25</p>
+      </div>
+    </div>
+  </details>
+  <details>
+    <summary><i class="fas fa-user-plus"></i> 加入我们</summary>
+    <div class="details-wrapper">
+      <div class="details-styling">    
+        <p>我们欢迎您加入Project Teuqila！</p>
+        <p>加入我们目前只有一种方法：</p>
+        <p><h3>邀请制</h3></p>
+        <p>经内部成员邀请而加入</p>
+        <p><h3>操作流程</h3></p>
+        <p>1. 加入Project Tequila群聊；</p>
+        <p>2. 联系管理员获得<a href="https://skin.w-m.fun" target="_blank">皮肤站</a>邀请码，注册皮肤站；</p>
+        <p>3.使用外置登录进入游戏。（我们也支持直接正版登录）</p>
+        <p>（皮肤站地址：<a href="https://skin.w-m.fun" target="_blank">https://skin.w-m.fun</a>）</p>
+        <p>（服务器地址请在群里获得）</p>
+      </div>
+    </div>
+  </details>
+  <details>
+    <summary><i class="fas fa-map-marked-alt"></i> 地图</summary>
+    <div class="details-wrapper">
+      <div class="details-styling">        
+        <p><b>在这里可以浏览最新的世界地图<sup>2021.02.23</sup></b><br><br>
+          <img src="src/map.png" align="center" width="340px"></p>
+        <p><a href="src/map.png" target="_blank"><i class="fas fa-image"></i> <b>查看大图</b></a></p>
+      </div>
+    </div>
+  </details>
+  <details>
+    <summary><i class="fas fa-download"></i> 下载</summary>
+    <div class="details-wrapper">
+      <div class="details-styling">        
+        <p>我们提供基于HMCL定制的多终端客户端</p>
+        <p>考虑到大文件下载压力较大，从新版本起将不提供完整安装包的直接下载选项，请使用客户端自行安装。</p>
+        <p>当然，我们会在第三方网盘中提供完整安装包（懒人包），你可以从那里下载。</p>
+        <p>我们仍提供光影、Mod的<a href="#recom">推荐</a>，且部分会在懒人包中附带安装。</p>
+        <p><h3>Windows, MacOS & Linux</h3></p>
+        <p><b>完整安装包<sup> 懒人包</sup></b></p>
+        <p><a href="https://pan.baidu.com/s/1ECm3H5E97n20upsmfxQgyg" target="_blank"><i class="fas fa-cloud-download-alt"></i> 百度网盘<sup>提取码: h8ub</sup></a></p>
+        <p><a href="https://share.weiyun.com/SYxfT6Rh" target="_blank"><i class="fas fa-cloud-download-alt"></i> 微云</a></p>
+        <p><h3>Android</h3></p>
+        <p>请自行配置<a href="https://github.com/AOF-Dev/MCinaBox" target="_blank">MCinaBox</a>或使用云电脑。</p>
+        
+      </div>
+    </div>
+  </details>
+  <details>
+    <summary><i class="fas fa-images"></i> 画廊</summary>
+    <div class="details-wrapper">
+      <div class="details-styling">        
+        <p>画廊收集了我们服务器的点点滴滴。</p>
+        <p>你可以在这里欣赏我们共同的作品。</p>
+        <p><a href="/picture"><i class="fas fa-location-arrow"></i> 前往画廊</a></p>
+      </div>
+    </div>
+  </details>
+  <details>
+    <summary><i class="fas fa-microchip" id="recom"></i> 配置推荐</summary>
+    <div class="details-wrapper" >
+      <div class="details-styling">        
+        <p><h3><code>//硬件配置</code></h3></p>
+        <p><h3>最低配置</h3></p>
+        <p>系统: Windows 7及以上(MacOS&Linux尚不清楚最低系统要求）</p>
+        <p>处理器：单核2.0Ghz</p>
+        <p>显卡:支持OpenGL 1.4</p>
+        <p>内存：1GB</p>
+        <p>硬盘：1GB</p>
+        <p>带宽：1Mbps</p>
+        <p><h3>推荐配置</h3></p>
+        <p>当然是越高越好！</p>
+        <p><h3><code>//软件配置</code></h3></p>
+        <p><h3>Java</h3></p>
+        <p>Java环境是游戏的基石，我们推荐使用64位的Java。（32位Java最大仅支持1GB内存占用）</p>
+        <p><h3>Optifine</h3></p>
+        <p>我们建议安装Optifine，这会显著改善多线程性能与游戏FPS，且光影基于Optifine。</p>
+        <p><h3>Forge/Farbic</h3></p>
+        <p>Forge/Farbic是Mod的基石。</p>
+        <p><h3>Mod</h3></p>
+        <p>我们在懒人包中集成了三个Mod，它们是：</p>
+        <p><b>CustomSkinLoader</b>：用于加载第三方皮肤</p>
+        <p><b>VoxelMap</b>：小地图插件</p>
+        <p><b>OptiFine</b>：画质优化与光影</p>
+        <p><h3>光影</h3></p>
+        <p>我们在懒人包中集成了<b>Sildurs Vibrant Shaders v1.28</b>，你可根据不同配置选择不同级别，默认为<b>Lite</b>。</p>
+        
+      </div>
+    </div>
+  </details>
+<!--</div>  
+<div class="container collapse">-->
+  <details>
+    <summary><i class="fas fa-donate"></i> 捐助</summary>
+    <div class="details-wrapper">
+      <div class="details-styling">        
+        <p>本服务器免费开放。</p>
+        <p>我们欢迎您的捐赠，这会让我们做得更好，您的慷慨也将被记录并致谢。</p>
+        <p><h3>特别致谢</h3></p>
+        <p>Again141 30元</p>
+        <p>tmk1373 20元</p>
+        <p>Kawakaze_CN 30元</p>
+      </div>
+    </div>
+  </details>
+</div>
+<p><input type="checkbox" id="accordion-toggle"><label for="accordion-toggle"> 关闭自动折叠 </label></p>
+
+<div class="container collapse">
+<details>
+    <summary><i class="fas fa-comments"></i> 留言墙</summary>
+    <div class="details-wrapper">
+      <div class="details-styling" >        
+            <p>
+            <?php
+				$counter=file_get_contents('message.txt',$counter);
+				echo "$counter";
+			?>
+			</p>
+			<p>
+			<form action='/messageio.php' method="post" align="center"> 
+				<font size="3"><code>< 昵 称 ></code></font><br>
+				<input  style="width:150px; background-color:#efe7fd; border:0px; border-radius:8px; height:30px;color:#8f63e9; font-size:19px; font-weight:700"type=text name='name' method='post'><br>
+				<font size="3"><code>< 留 言 ></code></font><br>
+				<input style="width:250px; background-color:#efe7fd; border:0px; border-radius:8px; height:30px; color:#8f63e9; font-size:16px; font-weight:700" name='message'></input>
+				<br/><br/>
+				<input type="submit" value="发 射" align="center"style="width:70px; background-color:#efe7fd; border:0px; border-radius:3px; height:30px; color:#8f63e9; font-size:17px; font-weight:700" />  
+			</form>  
+			</p>
+      </div>
+    </div>
+  </details>
+</div>
+
+<p><img src="/src/PT-logo.png" height="20px"><br>友情链接 <br>
+  <a href="https://skin.w-m.fun" target="_blank">皮肤站</a>
+  <a href="https://zh.moegirl.org.cn/Project_Tequila" target="_blank">WIKI</a>
+  <a href="http://anoms.top:81/" target="_blank">ANOMS</a>
+  <!--<a href="http://mc.w-m.fun/404.html" target="_blank">404</a>-->
+  
+</p>
+
+  <script src='https://cdn.jsdelivr.net/gh/javan/details-element-polyfill@master/dist/details-element-polyfill.js'></script>
+
+  
+
+<script  src="js/index.js"></script>
+
+
+    <div class="bottom" id="bottom" >
+	    <br><br><br><sup>Coded with ❤ by WaterMonster</sup>
+	</div>
+
+</body>
+
+</html>
