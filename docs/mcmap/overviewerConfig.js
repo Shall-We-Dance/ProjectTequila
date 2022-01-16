4bd1e4b3c9bc742b9d5827ddf0c2150818c62f30
ShallWeTalk <91458979+Shall-We-Dance@users.noreply.github.com>
2022-01-15 16:25:18 -0800

    update_maps

---
+++ docs/mcmap/overviewerConfig.js
@@ -0,0 +1,289 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "survival - overworld",
+        "16survival - overworld",
+        "16hard - overworld"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1642290767",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Survival Daytime",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "survival",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "survival - overworld",
+            "last_rendertime": 1642290578,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -128,
+                129,
+                -112
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -128,
+                129,
+                -112
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "16Survival Daytime",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "16survival",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "16survival - overworld",
+            "last_rendertime": 1642290447,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                275,
+                112,
+                129
+            ],
+            "minZoom": 0,
+            "spawn": [
+                275,
+                112,
+                129
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "16Survival Daytime",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "16survival",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "16survival - overworld",
+            "last_rendertime": 1642142167,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                275,
+                112,
+                129
+            ],
+            "minZoom": 0,
+            "spawn": [
+                275,
+                112,
+                129
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "16Survival Daytime",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "16survival",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "16survival - overworld",
+            "last_rendertime": 1642248975,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                275,
+                112,
+                129
+            ],
+            "minZoom": 0,
+            "spawn": [
+                275,
+                112,
+                129
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "16Survival Daytime",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "16survival",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "16survival - overworld",
+            "last_rendertime": 1642289080,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                275,
+                112,
+                129
+            ],
+            "minZoom": 0,
+            "spawn": [
+                275,
+                112,
+                129
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "16Survival Daytime",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "16survival",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "16survival - overworld",
+            "last_rendertime": 1642290448,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                275,
+                112,
+                129
+            ],
+            "minZoom": 0,
+            "spawn": [
+                275,
+                112,
+                129
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "16Hard Daytime",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "16hard",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "16hard - overworld",
+            "last_rendertime": 1642290371,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -62,
+                64,
+                67
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -62,
+                64,
+                67
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "16Hard Daytime",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "16hard",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "16hard - overworld",
+            "last_rendertime": 1642173086,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -62,
+                64,
+                67
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -62,
+                64,
+                67
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "16Hard Daytime",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "16hard",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "16hard - overworld",
+            "last_rendertime": 1642143641,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -62,
+                64,
+                67
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -62,
+                64,
+                67
+            ],
+            "north_direction": 0
+        }
+    ]
+};
